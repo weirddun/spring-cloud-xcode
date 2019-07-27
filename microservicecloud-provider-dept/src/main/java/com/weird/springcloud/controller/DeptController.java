24d98993a4cb17848b21ff254d85a0109076702d
weird_dun <weird_dun@foxmail.com>
2019-07-27 12:02:07 +0800

    修改了一些东西

--- microservicecloud-provider-dept/src/main/java/com/weird/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept/src/main/java/com/weird/springcloud/controller/DeptController.java
@@ -3,6 +3,8 @@ package com.weird.springcloud.controller;
 import com.weird.springcloud.entities.Dept;
 import com.weird.springcloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,6 +18,9 @@ public class DeptController {
 
     @Autowired
     private DeptService deptService;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @RequestMapping(value = "/dept/add", method = RequestMethod.POST)
     public boolean add(@RequestBody Dept dept){
@@ -32,4 +37,14 @@ public class DeptController {
         return deptService.list();
     }
 
+    @RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+    public Object discovery(){
+        List<String> list = discoveryClient.getServices();
+        System.out.println(list);
+        List<ServiceInstance> srvList = discoveryClient.getInstances("MICROSPRINGCLOUD-DEPT");
+        for(ServiceInstance element : srvList){
+            System.out.println(element.getHost() + element.getServiceId());
+        }
+        return this.discoveryClient;
+    }
 }
