c096260a15e060e90bd433460e87abccdb5ccc02
weird_dun <weird_dun@foxmail.com>
2019-07-31 09:55:33 +0800

    添加Eureka集群

---
+++ microservicecloud-eureka-7002/src/main/java/com/weird/springcloud/EurekaServerApp.java
@@ -0,0 +1,17 @@
+package com.weird.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author dengzw
+ * @date 2019/7/26
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApp.class, args);
+    }
+}
